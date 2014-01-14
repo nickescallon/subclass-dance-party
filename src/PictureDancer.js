fbe062c8150aef3d6fcbb8b08816c72c153f652f
Hack Reactor Students <students@hackreactor.com>
2014-01-14 12:48:04 -0800

    Adds div wrapper around pictureDancer span

--- src/PictureDancer.js
+++ src/PictureDancer.js
@@ -1,7 +1,9 @@
 var PictureDancer = function(top,left){
   Dancer.call(this, top, left, 200);
-  this.$node.addClass('picture');
-  this.$node.addClass('pointCollector');
+  this.$node = $('<div class="dancerContainer"><span class="dancer"></span></div>');
+  this.$node.css({'position':'fixed'});
+  this.$node.find('span').addClass('picture');
+  this.$node.find('span').addClass('pointCollector');
   this.danceMoveCount = 0;
   this.danceMove = 0;
   this.danceMovesBeforeMove = 20;
@@ -13,10 +15,10 @@ PictureDancer.prototype.constructor = PictureDancer;
 PictureDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
   var className = 'elaine'+this.danceMove;
-  this.$node.removeClass(className);
+  this.$node.find('span').removeClass(className);
   this.danceMove = (this.danceMove)%8+1;
   className = 'elaine'+this.danceMove;
-  this.$node.addClass(className)
+  this.$node.find('span').addClass(className)
   this.danceMoveCount++;
 
   if(this.danceMoveCount === this.danceMovesBeforeMove) {
