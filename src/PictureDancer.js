17f660e52a8d2d5d624b449d25c1e2b145769186
Hack Reactor Students <students@hackreactor.com>
2014-01-13 16:40:45 -0800

    Adds movement to Picture Dancers

--- src/PictureDancer.js
+++ src/PictureDancer.js
@@ -5,7 +5,26 @@ var PictureDancer = function(top,left){
 
 PictureDancer.prototype = Object.create(Dancer.prototype);
 PictureDancer.prototype.constructor = PictureDancer;
-PictureDancer.prototype.set = function(){
-  Dancer.prototype.set.call(this);
+PictureDancer.prototype.step = function(){
+  Dancer.prototype.step.call(this);
+  var urlString = this.background[Math.floor(Math.random()*6)];
+  var top = $("body").height() * Math.random();
+  var width = $("body").width() * Math.random();
+  this.$node.css({'background-image':"url('"+urlString+"')"});
+  this.$node.animate({
+    'top':top,
+    'left': width
+  }, 2000);
+
+  // console.log(urlString);
 
 }
+
+PictureDancer.prototype.background = [
+  'pics/1.png',
+  'pics/2.png',
+  'pics/3.png',
+  'pics/4.png',
+  'pics/5.png',
+  'pics/6.png'
+];
